d509282d225fec2cb4048c48b235e4a73ccbbff1
wetzernutz <126750514+wetzernutz@users.noreply.github.com>
2024-05-27 16:35:26 +1000

    Refactored to use absolute imports

---
+++ src/nodes/textnode.py
@@ -0,0 +1,42 @@
+from src.nodes.leafnode import LeafNode
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
